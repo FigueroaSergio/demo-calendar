282c7f6a2da5be302d5f8c5f89b8d9adf9f3f407
Sergio Figueroa <figueroa.sergio.26@gmail.com>
2026-04-23 16:32:29 +0200

    CHANGE name

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/sergio.figueroa/demo-calendar/",
+  base: "/demo-calendar/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
