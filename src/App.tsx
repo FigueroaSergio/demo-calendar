4d75a66543f2a1cb803acbedb26b8a9f470ae044
Sergio Figueroa <figueroa.sergio.26@gmail.com>
2026-04-23 17:00:54 +0200

    FIX git-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { MainLayout } from './components/layout/MainLayout';
-import { PatientPortal } from './features/patient/PatientPortal';
-import { ProviderSearch } from './features/patient/ProviderSearch';
-import { BookingWizard } from './features/patient/BookingWizard';
-import { AdminDashboard } from './features/admin/AdminDashboard';
-import { DoctorsPage } from './features/admin/doctors/DoctorsPage';
-import { DoctorFormPage } from './features/admin/doctors/DoctorFormPage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { MainLayout } from "./components/layout/MainLayout";
+import { PatientPortal } from "./features/patient/PatientPortal";
+import { ProviderSearch } from "./features/patient/ProviderSearch";
+import { BookingWizard } from "./features/patient/BookingWizard";
+import { AdminDashboard } from "./features/admin/AdminDashboard";
+import { DoctorsPage } from "./features/admin/doctors/DoctorsPage";
+import { DoctorFormPage } from "./features/admin/doctors/DoctorFormPage";
 
 function AppRouter() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/demo-calendar">
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<ProviderSearch />} />
@@ -26,9 +26,7 @@ function AppRouter() {
 }
 
 function App() {
-  return (
-    <AppRouter />
-  );
+  return <AppRouter />;
 }
 
 export default App;
